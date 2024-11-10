4d7751dae76d4e266a66e72e8197df1d497b4d5e
n-sld-44 <nathanael.soulard@edu.devinci.fr>
2024-11-10 22:38:34 +0100

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NodeReact_Workshop';
+  constructor() {
+    console.log('AppComponent on init');
+  }
+  ngOnInit() {
+    console.log('AppComponent');
+  }
 }
